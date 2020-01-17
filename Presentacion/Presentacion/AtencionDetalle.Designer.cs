21d04a0104592355396185400d9ef46e322b2324
CarlosIza <46698062+CarlosIza@users.noreply.github.com>
2020-01-17 00:04:59 -0500

    detalle atencion

---
+++ Presentacion/Presentacion/AtencionDetalle.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace Presentacion
+{
+	partial class AtencionDetalle
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.label10 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.btnCerrar = new System.Windows.Forms.Button();
+			this.btnMinimizar = new System.Windows.Forms.Button();
+			this.panel1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// panel1
+			// 
+			this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(70)))), ((int)(((byte)(87)))), ((int)(((byte)(80)))));
+			this.panel1.Controls.Add(this.label10);
+			this.panel1.Controls.Add(this.label2);
+			this.panel1.Controls.Add(this.btnCerrar);
+			this.panel1.Controls.Add(this.btnMinimizar);
+			this.panel1.Location = new System.Drawing.Point(12, 12);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(729, 84);
+			this.panel1.TabIndex = 33;
+			// 
+			// label10
+			// 
+			this.label10.AutoSize = true;
+			this.label10.BackColor = System.Drawing.Color.Transparent;
+			this.label10.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label10.ForeColor = System.Drawing.Color.White;
+			this.label10.Location = new System.Drawing.Point(11, 37);
+			this.label10.Name = "label10";
+			this.label10.Size = new System.Drawing.Size(0, 19);
+			this.label10.TabIndex = 35;
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Font = new System.Drawing.Font("Century Gothic", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label2.ForeColor = System.Drawing.Color.White;
+			this.label2.Location = new System.Drawing.Point(234, 22);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(258, 34);
+			this.label2.TabIndex = 11;
+			this.label2.Text = "Registrar Paciente";
+			// 
+			// btnCerrar
+			// 
+			this.btnCerrar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+			this.btnCerrar.FlatAppearance.BorderSize = 0;
+			this.btnCerrar.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(28)))), ((int)(((byte)(28)))));
+			this.btnCerrar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+			this.btnCerrar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.btnCerrar.Font = new System.Drawing.Font("Segoe Script", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btnCerrar.ForeColor = System.Drawing.Color.LightBlue;
+			this.btnCerrar.Location = new System.Drawing.Point(671, 10);
+			this.btnCerrar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+			this.btnCerrar.Name = "btnCerrar";
+			this.btnCerrar.Size = new System.Drawing.Size(36, 28);
+			this.btnCerrar.TabIndex = 17;
+			this.btnCerrar.Text = "X";
+			this.btnCerrar.UseVisualStyleBackColor = false;
+			// 
+			// btnMinimizar
+			// 
+			this.btnMinimizar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+			this.btnMinimizar.FlatAppearance.BorderSize = 0;
+			this.btnMinimizar.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(28)))), ((int)(((byte)(28)))));
+			this.btnMinimizar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+			this.btnMinimizar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.btnMinimizar.Font = new System.Drawing.Font("Segoe Script", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btnMinimizar.ForeColor = System.Drawing.Color.LightGray;
+			this.btnMinimizar.Location = new System.Drawing.Point(629, 10);
+			this.btnMinimizar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+			this.btnMinimizar.Name = "btnMinimizar";
+			this.btnMinimizar.Size = new System.Drawing.Size(36, 28);
+			this.btnMinimizar.TabIndex = 18;
+			this.btnMinimizar.Text = "-";
+			this.btnMinimizar.UseVisualStyleBackColor = false;
+			// 
+			// AtencionDetalle
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.panel1);
+			this.Name = "AtencionDetalle";
+			this.Text = "AtencionDetalle";
+			this.panel1.ResumeLayout(false);
+			this.panel1.PerformLayout();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.Label label10;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Button btnCerrar;
+		private System.Windows.Forms.Button btnMinimizar;
+	}
+}
