1e31cad0e44c79bbf60a2897b43842d14ceba709
CarlosIza <46698062+CarlosIza@users.noreply.github.com>
2020-01-17 00:03:03 -0500

    creacion paciente (#1)

---
+++ Presentacion/Presentacion/Atencion.Designer.cs
@@ -0,0 +1,389 @@
+﻿namespace Presentacion
+{
+	partial class Atencion
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.label10 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.btnCerrar = new System.Windows.Forms.Button();
+			this.btnMinimizar = new System.Windows.Forms.Button();
+			this.cbGenero = new System.Windows.Forms.ComboBox();
+			this.txtFechaIngresPac = new System.Windows.Forms.TextBox();
+			this.txtFechaNac = new System.Windows.Forms.TextBox();
+			this.label9 = new System.Windows.Forms.Label();
+			this.txtDireccion = new System.Windows.Forms.TextBox();
+			this.label8 = new System.Windows.Forms.Label();
+			this.txtTelefono = new System.Windows.Forms.TextBox();
+			this.dtFechaIngreso = new System.Windows.Forms.DateTimePicker();
+			this.label7 = new System.Windows.Forms.Label();
+			this.txtCedula = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label6 = new System.Windows.Forms.Label();
+			this.label5 = new System.Windows.Forms.Label();
+			this.dtFechaNacimiento = new System.Windows.Forms.DateTimePicker();
+			this.txtNombre1 = new System.Windows.Forms.TextBox();
+			this.txtApellido1 = new System.Windows.Forms.TextBox();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.panel1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// panel1
+			// 
+			this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(70)))), ((int)(((byte)(87)))), ((int)(((byte)(80)))));
+			this.panel1.Controls.Add(this.label10);
+			this.panel1.Controls.Add(this.label2);
+			this.panel1.Controls.Add(this.btnCerrar);
+			this.panel1.Controls.Add(this.btnMinimizar);
+			this.panel1.Location = new System.Drawing.Point(2, 3);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(976, 84);
+			this.panel1.TabIndex = 33;
+			// 
+			// label10
+			// 
+			this.label10.AutoSize = true;
+			this.label10.BackColor = System.Drawing.Color.Transparent;
+			this.label10.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label10.ForeColor = System.Drawing.Color.White;
+			this.label10.Location = new System.Drawing.Point(11, 37);
+			this.label10.Name = "label10";
+			this.label10.Size = new System.Drawing.Size(0, 19);
+			this.label10.TabIndex = 35;
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Font = new System.Drawing.Font("Century Gothic", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label2.ForeColor = System.Drawing.Color.White;
+			this.label2.Location = new System.Drawing.Point(394, 22);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(139, 34);
+			this.label2.TabIndex = 11;
+			this.label2.Text = "Atenciòn";
+			// 
+			// btnCerrar
+			// 
+			this.btnCerrar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+			this.btnCerrar.FlatAppearance.BorderSize = 0;
+			this.btnCerrar.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(28)))), ((int)(((byte)(28)))));
+			this.btnCerrar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+			this.btnCerrar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.btnCerrar.Font = new System.Drawing.Font("Segoe Script", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btnCerrar.ForeColor = System.Drawing.Color.LightBlue;
+			this.btnCerrar.Location = new System.Drawing.Point(935, 2);
+			this.btnCerrar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+			this.btnCerrar.Name = "btnCerrar";
+			this.btnCerrar.Size = new System.Drawing.Size(36, 28);
+			this.btnCerrar.TabIndex = 17;
+			this.btnCerrar.Text = "X";
+			this.btnCerrar.UseVisualStyleBackColor = false;
+			// 
+			// btnMinimizar
+			// 
+			this.btnMinimizar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+			this.btnMinimizar.FlatAppearance.BorderSize = 0;
+			this.btnMinimizar.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(28)))), ((int)(((byte)(28)))));
+			this.btnMinimizar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+			this.btnMinimizar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.btnMinimizar.Font = new System.Drawing.Font("Segoe Script", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btnMinimizar.ForeColor = System.Drawing.Color.LightGray;
+			this.btnMinimizar.Location = new System.Drawing.Point(893, 2);
+			this.btnMinimizar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+			this.btnMinimizar.Name = "btnMinimizar";
+			this.btnMinimizar.Size = new System.Drawing.Size(36, 28);
+			this.btnMinimizar.TabIndex = 18;
+			this.btnMinimizar.Text = "-";
+			this.btnMinimizar.UseVisualStyleBackColor = false;
+			// 
+			// cbGenero
+			// 
+			this.cbGenero.Font = new System.Drawing.Font("Century Gothic", 10.2F);
+			this.cbGenero.FormattingEnabled = true;
+			this.cbGenero.Items.AddRange(new object[] {
+            "M",
+            "F"});
+			this.cbGenero.Location = new System.Drawing.Point(372, 282);
+			this.cbGenero.Name = "cbGenero";
+			this.cbGenero.Size = new System.Drawing.Size(143, 29);
+			this.cbGenero.TabIndex = 45;
+			// 
+			// txtFechaIngresPac
+			// 
+			this.txtFechaIngresPac.BackColor = System.Drawing.Color.White;
+			this.txtFechaIngresPac.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.txtFechaIngresPac.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.txtFechaIngresPac.ForeColor = System.Drawing.SystemColors.WindowText;
+			this.txtFechaIngresPac.Location = new System.Drawing.Point(372, 426);
+			this.txtFechaIngresPac.Name = "txtFechaIngresPac";
+			this.txtFechaIngresPac.Size = new System.Drawing.Size(143, 28);
+			this.txtFechaIngresPac.TabIndex = 53;
+			// 
+			// txtFechaNac
+			// 
+			this.txtFechaNac.BackColor = System.Drawing.Color.White;
+			this.txtFechaNac.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.txtFechaNac.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.txtFechaNac.ForeColor = System.Drawing.SystemColors.WindowText;
+			this.txtFechaNac.Location = new System.Drawing.Point(372, 235);
+			this.txtFechaNac.Name = "txtFechaNac";
+			this.txtFechaNac.Size = new System.Drawing.Size(143, 28);
+			this.txtFechaNac.TabIndex = 52;
+			// 
+			// label9
+			// 
+			this.label9.AutoSize = true;
+			this.label9.BackColor = System.Drawing.Color.Transparent;
+			this.label9.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label9.ForeColor = System.Drawing.Color.White;
+			this.label9.Location = new System.Drawing.Point(264, 381);
+			this.label9.Name = "label9";
+			this.label9.Size = new System.Drawing.Size(93, 19);
+			this.label9.TabIndex = 51;
+			this.label9.Text = "Dirección:";
+			// 
+			// txtDireccion
+			// 
+			this.txtDireccion.BackColor = System.Drawing.Color.White;
+			this.txtDireccion.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.txtDireccion.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.txtDireccion.ForeColor = System.Drawing.SystemColors.WindowText;
+			this.txtDireccion.Location = new System.Drawing.Point(372, 378);
+			this.txtDireccion.Name = "txtDireccion";
+			this.txtDireccion.Size = new System.Drawing.Size(425, 28);
+			this.txtDireccion.TabIndex = 50;
+			// 
+			// label8
+			// 
+			this.label8.AutoSize = true;
+			this.label8.BackColor = System.Drawing.Color.Transparent;
+			this.label8.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label8.ForeColor = System.Drawing.Color.White;
+			this.label8.Location = new System.Drawing.Point(273, 332);
+			this.label8.Name = "label8";
+			this.label8.Size = new System.Drawing.Size(84, 19);
+			this.label8.TabIndex = 49;
+			this.label8.Text = "Teléfono:";
+			// 
+			// txtTelefono
+			// 
+			this.txtTelefono.BackColor = System.Drawing.Color.White;
+			this.txtTelefono.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.txtTelefono.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.txtTelefono.ForeColor = System.Drawing.SystemColors.WindowText;
+			this.txtTelefono.Location = new System.Drawing.Point(372, 329);
+			this.txtTelefono.Name = "txtTelefono";
+			this.txtTelefono.Size = new System.Drawing.Size(143, 28);
+			this.txtTelefono.TabIndex = 48;
+			// 
+			// dtFechaIngreso
+			// 
+			this.dtFechaIngreso.Font = new System.Drawing.Font("Century Gothic", 10.2F);
+			this.dtFechaIngreso.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+			this.dtFechaIngreso.Location = new System.Drawing.Point(521, 426);
+			this.dtFechaIngreso.Name = "dtFechaIngreso";
+			this.dtFechaIngreso.Size = new System.Drawing.Size(148, 28);
+			this.dtFechaIngreso.TabIndex = 47;
+			// 
+			// label7
+			// 
+			this.label7.AutoSize = true;
+			this.label7.BackColor = System.Drawing.Color.Transparent;
+			this.label7.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label7.ForeColor = System.Drawing.Color.White;
+			this.label7.Location = new System.Drawing.Point(184, 430);
+			this.label7.Name = "label7";
+			this.label7.Size = new System.Drawing.Size(173, 19);
+			this.label7.TabIndex = 46;
+			this.label7.Text = "Fecha Ing Paciente:";
+			// 
+			// txtCedula
+			// 
+			this.txtCedula.BackColor = System.Drawing.Color.White;
+			this.txtCedula.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.txtCedula.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.txtCedula.ForeColor = System.Drawing.SystemColors.WindowText;
+			this.txtCedula.Location = new System.Drawing.Point(372, 96);
+			this.txtCedula.Name = "txtCedula";
+			this.txtCedula.Size = new System.Drawing.Size(297, 28);
+			this.txtCedula.TabIndex = 34;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.BackColor = System.Drawing.Color.Transparent;
+			this.label1.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.ForeColor = System.Drawing.Color.White;
+			this.label1.Location = new System.Drawing.Point(286, 96);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(74, 19);
+			this.label1.TabIndex = 36;
+			this.label1.Text = "Cédula:";
+			// 
+			// label6
+			// 
+			this.label6.AutoSize = true;
+			this.label6.BackColor = System.Drawing.Color.Transparent;
+			this.label6.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label6.ForeColor = System.Drawing.Color.White;
+			this.label6.Location = new System.Drawing.Point(281, 287);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(76, 19);
+			this.label6.TabIndex = 44;
+			this.label6.Text = "Género:";
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.BackColor = System.Drawing.Color.Transparent;
+			this.label5.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label5.ForeColor = System.Drawing.Color.White;
+			this.label5.Location = new System.Drawing.Point(252, 238);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(105, 19);
+			this.label5.TabIndex = 35;
+			this.label5.Text = "Fecha Nac:";
+			// 
+			// dtFechaNacimiento
+			// 
+			this.dtFechaNacimiento.Font = new System.Drawing.Font("Century Gothic", 10.2F);
+			this.dtFechaNacimiento.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+			this.dtFechaNacimiento.Location = new System.Drawing.Point(521, 235);
+			this.dtFechaNacimiento.Name = "dtFechaNacimiento";
+			this.dtFechaNacimiento.Size = new System.Drawing.Size(148, 28);
+			this.dtFechaNacimiento.TabIndex = 43;
+			// 
+			// txtNombre1
+			// 
+			this.txtNombre1.BackColor = System.Drawing.Color.White;
+			this.txtNombre1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.txtNombre1.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.txtNombre1.ForeColor = System.Drawing.SystemColors.WindowText;
+			this.txtNombre1.Location = new System.Drawing.Point(372, 188);
+			this.txtNombre1.Name = "txtNombre1";
+			this.txtNombre1.Size = new System.Drawing.Size(143, 28);
+			this.txtNombre1.TabIndex = 38;
+			// 
+			// txtApellido1
+			// 
+			this.txtApellido1.BackColor = System.Drawing.Color.White;
+			this.txtApellido1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.txtApellido1.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.txtApellido1.ForeColor = System.Drawing.SystemColors.WindowText;
+			this.txtApellido1.Location = new System.Drawing.Point(372, 142);
+			this.txtApellido1.Name = "txtApellido1";
+			this.txtApellido1.Size = new System.Drawing.Size(143, 28);
+			this.txtApellido1.TabIndex = 37;
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.BackColor = System.Drawing.Color.Transparent;
+			this.label3.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label3.ForeColor = System.Drawing.Color.White;
+			this.label3.Location = new System.Drawing.Point(271, 190);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(89, 19);
+			this.label3.TabIndex = 39;
+			this.label3.Text = "Nombres:";
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.BackColor = System.Drawing.Color.Transparent;
+			this.label4.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label4.ForeColor = System.Drawing.Color.White;
+			this.label4.Location = new System.Drawing.Point(271, 142);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(91, 19);
+			this.label4.TabIndex = 40;
+			this.label4.Text = "Apellidos:";
+			// 
+			// Atencion
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(57)))), ((int)(((byte)(80)))));
+			this.ClientSize = new System.Drawing.Size(1008, 551);
+			this.Controls.Add(this.cbGenero);
+			this.Controls.Add(this.txtFechaIngresPac);
+			this.Controls.Add(this.txtFechaNac);
+			this.Controls.Add(this.label9);
+			this.Controls.Add(this.txtDireccion);
+			this.Controls.Add(this.label8);
+			this.Controls.Add(this.txtTelefono);
+			this.Controls.Add(this.dtFechaIngreso);
+			this.Controls.Add(this.label7);
+			this.Controls.Add(this.txtCedula);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.label6);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.dtFechaNacimiento);
+			this.Controls.Add(this.txtNombre1);
+			this.Controls.Add(this.txtApellido1);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.panel1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+			this.Name = "Atencion";
+			this.Text = "Form2";
+			this.panel1.ResumeLayout(false);
+			this.panel1.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.Label label10;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Button btnCerrar;
+		private System.Windows.Forms.Button btnMinimizar;
+		public System.Windows.Forms.ComboBox cbGenero;
+		public System.Windows.Forms.TextBox txtFechaIngresPac;
+		public System.Windows.Forms.TextBox txtFechaNac;
+		private System.Windows.Forms.Label label9;
+		public System.Windows.Forms.TextBox txtDireccion;
+		private System.Windows.Forms.Label label8;
+		public System.Windows.Forms.TextBox txtTelefono;
+		private System.Windows.Forms.DateTimePicker dtFechaIngreso;
+		private System.Windows.Forms.Label label7;
+		public System.Windows.Forms.TextBox txtCedula;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.DateTimePicker dtFechaNacimiento;
+		public System.Windows.Forms.TextBox txtNombre1;
+		public System.Windows.Forms.TextBox txtApellido1;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label4;
+	}
+}
